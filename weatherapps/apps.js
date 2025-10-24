143c28fcd41cd1b0d636c3202b3814348e845eff
developershubhamsingh <shubhamsinghworkid@gmail.com>
2025-10-24 12:45:46 -0700

    .

--- weatherapps/apps.js
+++ weatherapps/apps.js
@@ -4,13 +4,14 @@ let dotenv = require('dotenv');
 dotenv.config();
 let apps = express();
 let port = process.env.PORT || 9800;
+
 apps.use(express.static(__dirname + "/public"));
 apps.set("views", "./src/views");
 apps.set("view engine", "ejs");
 
 apps.get("/weather", (req, res) => {
     let city = req.query.city ? req.query.city : "Delhi";
-    let url = ` https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&mode=json&units=metric&cnt=5&appid=${process.env.KEY} `;
+    let url = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&mode=json&units=metric&cnt=5&appid=${process.env.KEY}`;
 
     request(url, (error, response) => {
         if (error) throw error;
